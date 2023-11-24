52825ff7cbd807b053655372e8e20ba923bf9587
AzamatMl <33450014+MlAzamat@users.noreply.github.com>
2023-11-24 22:12:30 +0300

    add generator and scripts and scenario for mock

---
+++ test_mock_server/ScenarioMaxPerf.js
@@ -0,0 +1,83 @@
+import UC01_get_greeting from './scripts/UC01_get_greeting.js';
+import UC02_post_users from './scripts/UC02_post_users.js';
+// import UC03_CSES_fts_v1_debtor_info from 'C:/Nikolaenko/K6/scripts/1550/UC03_CSES_fts_v1_debtor_info.js';
+
+
+
+export const options = {
+  scenarios: {
+    UC01_get_greeting: {
+      executor: 'ramping-arrival-rate',
+      startTime: '2s', // the ramping API test starts a little later
+      // startRate: 1,
+      timeUnit: '1s', // we start at 50 iterations per second
+      preAllocatedVUs: 5,
+      maxVUs: 10,
+      stages: [
+        { target: 10, duration: '10s' }, // go from 50 to 200 iters/s in the first 30 seconds
+        { target: 10, duration: '1m' }, // hold at 200 iters/s for 3.5 minutes
+        { target: 20, duration: '10s' },
+        { target: 20, duration: '1m' },
+        { target: 30, duration: '10s' },
+        { target: 30, duration: '1m' },
+        { target: 0, duration: '10s' }
+      ]
+      // preAllocatedVUs: 50, // how large the initial pool of VUs would be
+      // maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more
+      //tags: { test_type: 'api' }, // different extra metric tags for this scenario
+      //env: { MY_CROC_ID: '2' }, // same function, different environment variables
+      //exec: 'apitest', // same function as the scenario above, but with different env vars
+    },
+    UC02_post_users: {
+      executor: 'ramping-arrival-rate',
+      startTime: '2s', // the ramping API test starts a little later
+      // startRate: 1,
+      timeUnit: '1s', // we start at 50 iterations per second
+      preAllocatedVUs: 5,
+      maxVUs: 10,
+      stages: [
+        { target: 15, duration: '10s' }, // go from 50 to 200 iters/s in the first 30 seconds
+        { target: 15, duration: '1m' }, // hold at 200 iters/s for 3.5 minutes
+        { target: 25, duration: '10s' },
+        { target: 25, duration: '1m' },
+        { target: 35, duration: '10s' },
+        { target: 35, duration: '1m' },
+        { target: 0, duration: '10s' }
+      ]
+    //   preAllocatedVUs: 50, // how large the initial pool of VUs would be
+    //   maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more
+    //   //tags: { test_type: 'api' }, // different extra metric tags for this scenario
+    //   //env: { MY_CROC_ID: '2' }, // same function, different environment variables
+    //   //exec: 'apitest', // same function as the scenario above, but with different env vars
+    // },
+    // UC03_CSES_fts_v1_debtor_info: {
+    //   executor: 'ramping-arrival-rate',
+    //   startTime: '10s', // the ramping API test starts a little later
+    //   startRate: 5,
+    //   timeUnit: '1s', // we start at 50 iterations per second
+    //   stages: [
+    //     { target: 20, duration: '10s' }, // go from 50 to 200 iters/s in the first 30 seconds
+    //     { target: 20, duration: '2m' }, // hold at 200 iters/s for 3.5 minutes
+    //     { target: 40, duration: '10s' },
+    //     { target: 40, duration: '2m' },
+    //     { target: 0, duration: '10s' }, // ramp down back to 0 iters/s over the last 30 second
+    //   ],
+    //   preAllocatedVUs: 50, // how large the initial pool of VUs would be
+    //   maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more
+    //   //tags: { test_type: 'api' }, // different extra metric tags for this scenario
+    //   //env: { MY_CROC_ID: '2' }, // same function, different environment variables
+    //   //exec: 'apitest', // same function as the scenario above, but with different env vars
+     },
+  },
+};
+
+// export function all() {
+//   UC01_CSES_fns_v1_egr_info();
+//   // UC02_CSES_rosstat_v1_egr_info();
+//   // UC03_CSES_fts_v1_debtor_info();
+// }
+
+export default function () {
+  UC01_get_greeting();
+  UC02_post_users();
+}
---
+++ test_mock_server/scripts/generator.js
@@ -0,0 +1,159 @@
+export function generate_inn(organization_type) {
+
+    var inn = [];
+
+    if (organization_type === "UL") {
+
+        for (var i = 0; i < 9; i++) {
+
+           inn.push(Math.floor(Math.random() * 10));
+
+        }
+
+        if ([90, 93, 94, 95, 96, 98].includes(inn[0] * 10 + inn[1])) {
+
+            inn[0] = inn[0] - 1;
+
+        }
+
+        inn.push(((inn[0] * 2 + inn[1] * 4 + inn[2] * 10 + inn[3] * 3 + inn[4] * 5 +
+
+                   inn[5] * 9 + inn[6] * 4 + inn[7] * 6 + inn[8] * 8) % 11) % 10);
+
+        } else if (organization_type === "FL") {
+
+            for (var i = 0; i < 10; i++) {
+
+                inn.push(Math.floor(Math.random() * 10));
+
+            }
+
+            if ([90, 93, 94, 95, 96, 98].includes(inn[0] * 10 + inn[1])) {
+
+                inn[0] = inn[0] - 1;
+
+            }
+
+            inn.push(((inn[0] * 7 + inn[1] * 2 + inn[2] * 4 + inn[3] * 10 + inn[4] * 3 +
+
+                       inn[5] * 5 + inn[6] * 9 + inn[7] * 4 + inn[8] * 6 + inn[9] * 8) % 11) % 10);
+
+            inn.push(((inn[0] * 3 + inn[1] * 7 + inn[2] * 2 + inn[3] * 4 + inn[4] * 10 +
+
+                       inn[5] * 3 + inn[6] * 5 + inn[7] * 9 + inn[8] * 4 + inn[9] * 6 + inn[10] * 8) % 11) % 10);
+
+        }
+
+   
+
+          
+
+ 
+
+    //console.log(inn.join(''));          
+
+    return inn.join('');
+
+}
+
+export function generate_ogrn(organization_type) {
+
+    if (organization_type === 'UL') {
+
+        var s1 = Math.floor(Math.random() * 2) + 1;
+
+        var s2_3 = new Date().getFullYear().toString().slice(2);
+
+        var s4_5 = 50;
+
+        var s6_12 = Math.floor(Math.random() * (9999999 - 1000000 + 1)) + 1000000;
+
+        var s1_12 = s1.toString() + s2_3 + s4_5 + s6_12.toString();
+
+        var s13 = parseInt(s1_12) % 11;
+
+        if (s13 === 10) {
+
+            s13 = 0;
+
+        }
+
+        //console.log(s1_12.toString() + s13.toString());
+
+        return s1_12.toString() + s13.toString();
+
+    } else if (organization_type === 'FL') {
+
+        var s1 = 3;
+
+        var s2_3 = new Date().getFullYear().toString().slice(2);
+
+        var s4_5 = 50;
+
+        var s6_14 = Math.floor(Math.random() * (999999999 - 100000000 + 1)) + 100000000;
+
+        var s1_14 = s1.toString() + s2_3 + s4_5 + s6_14.toString();
+
+        var s15 = parseInt(s1_14) % 13;
+
+        if (s15 === 10) {
+
+            s15 = 0;
+
+        }
+
+        //console.log(s1_14.toString() + s15.toString());
+
+        return s1_14.toString() + s15.toString();
+
+    } else {
+
+        throw new Error(organization_type);
+
+    }            
+
+}
+
+export function generate_kpp() {
+
+    var s1_2 = [50, 77, 90, 99][Math.floor(Math.random() * 4)];
+
+    var s3_4 = Math.floor(Math.random() * (99 - 10 + 1)) + 10;
+
+    var s5_6 = Math.floor(Math.random() * (50 - 10 + 1)) + 10;
+
+    var s7_9 = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+
+    return '' + s1_2 + s3_4 + s5_6 + s7_9;
+
+}
+
+ 
+
+export function generate_okpo(organization_type) {
+
+    var okpo = [];
+
+    if (organization_type === 'UL') {
+
+        okpo.push(Math.floor(Math.random() * 9) + 1);
+
+        for (var i = 0; i < 6; i++) {
+
+            okpo.push(Math.floor(Math.random() * 10));
+
+        }
+
+        okpo.push((okpo[0] * 1 + okpo[1] * 2 + okpo[2] * 3 + okpo[3] * 4 + okpo[4] * 5 +
+
+            okpo[5] * 6 + okpo[6] * 7) % 11);
+
+    } else if (organization_type === 'FL') {
+
+        return '' + Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) + 1000000000;
+
+    }
+
+    return okpo.join('');
+
+}
